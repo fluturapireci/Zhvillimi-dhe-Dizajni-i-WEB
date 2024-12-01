d53f172ba78a106238440e17bf15c5eb26db8a3c
erionrugova <erionrugova0@gmail.com>
2024-12-01 22:10:03 +0100

    Update product.js

--- js/product.js
+++ js/product.js
@@ -2,14 +2,15 @@ const productButton = document.getElementById("product-button");
 const dropdownMenu = document.getElementById("dropdown-menu");
 
 productButton.addEventListener("click", (event) => {
-    event.preventDefault(); // Prevents default link behavior
+    event.preventDefault(); r
     console.log("Products button clicked");
     dropdownMenu.classList.toggle("show");
 });
 
-// Close the dropdown if clicking outside
+
 document.addEventListener("click", (event) => {
     if (!dropdownMenu.contains(event.target) && !productButton.contains(event.target)) {
         dropdownMenu.classList.remove("show");
     }
 });
+
